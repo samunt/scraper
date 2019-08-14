4de274494aa6a16ea5cc75c98616b757169f2f48
Sam Unterman <samunterman@Sams-MacBook-Pro.local>
2019-08-14 11:51:59 -0400

    fixed file name

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/{{angular-forms-and-validations}}'));
+app.use(express.static('./dist/angular-forms-and-validations'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname,'/dist/{{angular-forms-and-validations}}/index.html'));
+  res.sendFile(path.join(__dirname,'/dist/angular-forms-and-validations/index.html'));
 });
 
 // Start the app by listening on the default Heroku port

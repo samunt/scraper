52aadf3aba1b1cf8a4a5bc062b1c3453c83da94c
Sam Unterman <samunterman@Sams-MacBook-Pro.local>
2019-08-16 14:44:00 -0400

    Working

--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -27,9 +27,12 @@ export class FormsComponent implements OnInit {
 
   public onSubmitUserDetails(value) {
     let kinetixResp;
-    const findDollar = new RegExp('/\\$\\d+(?:(\\d+))?/g');
+    const findDollar = new RegExp('\\$\\d+(?:(\\d+))?', 'g');
+    const premium = value.premium;
     let post: Observable<any>;
-    const httpBody = 'AID=term_life_step_one&quoter=life&quote_ref_id=KL190813-6M3HJ&session_id=53616c7465645f5f6cea3028ef90bce027a16db6ff1a837b45beba5bb8f3ac642c2d52a118df2515&State=1&Health=P&partner_Health=P&coverage_type=term&modify=&province=ON&theme_city+value=&theme_province=&postal_code=M3H4M3&joint=single&premium=250000&term_single=1C&term_joint=1C&savers_email=jondoe%40gmail.com&birth_month=04&birth_day=05&birth_year=1990&gender=M&smoker=N&cigarette=0&cigar=0&cigarello=0&pipe=0&chew=0&nicotine=0&marijuana=0&smoke_work=0&smoke_home=0&partner_birth_month=&partner_birth_day=&partner_birth_year=&partner_cigarette=0&partner_cigar=0&partner_cigarello=0&partner_pipe=0&partner_chew=0&partner_nicotine=0&partner_marijuana=0&partner_smoke_work=0&partner_smoke_home=0';
+    const httpBody = 'AID=term_life_step_one&quoter=life&quote_ref_id=KL190813-6M3HJ&session_id=53616c7465645f5f6cea3028ef90bce027a16db6ff1a837b45beba5bb8f3ac642c2d52a118df2515&State=1&Health=P&partner_Health=P&coverage_type=term&modify=&province=ON&theme_city+value=&theme_province=&postal_code=M3H4M3&joint=single&premium=';
+    const endOfBody = '&term_single=1C&term_joint=1C&savers_email=jondoe%40gmail.com&birth_month=04&birth_day=05&birth_year=1990&gender=M&smoker=N&cigarette=0&cigar=0&cigarello=0&pipe=0&chew=0&nicotine=0&marijuana=0&smoke_work=0&smoke_home=0&partner_birth_month=&partner_birth_day=&partner_birth_year=&partner_cigarette=0&partner_cigar=0&partner_cigarello=0&partner_pipe=0&partner_chew=0&partner_nicotine=0&partner_marijuana=0&partner_smoke_work=0&partner_smoke_home=0';
+    const constructedBody = httpBody + premium + endOfBody;
     const destinationUrl = 'https://partners.kanetix.ca/life-insurance';
     const httpOptions = {
       headers: new HttpHeaders({
@@ -37,15 +40,17 @@ export class FormsComponent implements OnInit {
       }),
       mode: "cors",
     };
-    console.log('build 8888');
-    post = this.httpClient.post(destinationUrl, httpBody, httpOptions);
+    console.log('build 9999', value);
+    post = this.httpClient.post(destinationUrl, constructedBody, httpOptions);
     post.subscribe(
       (response) => {
          console.log('response body', response);
     }, (err) => {
          kinetixResp = err.error.text;
+         console.log('error', err);
          kinetixResp = kinetixResp.match(findDollar);
          console.log('DOLLAR VAL', kinetixResp);
+         alert('Lowest quote for Empire on Kinetix for premium ' + premium.toString() + ' is ' + kinetixResp[0]);
     });
 
   }

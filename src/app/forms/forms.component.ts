4713efd07efcc9cfea36ee61dd56d36b3f9fc081
Sam <sammy.unterman@gmail.com>
2019-08-28 10:55:25 -0400

    wip

--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -1,15 +1,15 @@
 // tslint:disable-next-line:import-spacing
-import {HttpClient, HttpHeaders} from '@angular/common/http';
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 // import { parse } from 'node-html-parser';
-import parse from 'html-dom-parser';
-import {Observable} from 'rxjs';
+import parse from "html-dom-parser";
+import {Observable} from "rxjs";
 @Component({
-  selector: 'app-forms-page',
-  templateUrl: './forms.component.html',
-  styleUrls: ['./forms.component.scss'],
-  encapsulation: ViewEncapsulation.None
+  selector: "app-forms-page",
+  templateUrl: "./forms.component.html",
+  styleUrls: ["./forms.component.scss"],
+  encapsulation: ViewEncapsulation.None,
 })
 export class FormsComponent implements OnInit {
 
@@ -21,23 +21,240 @@ export class FormsComponent implements OnInit {
   }
 
   public createForms() {
+    const coverageTypeTable = [
+      {
+        type: "1 Year Term",
+        val: "00",
+      },
+      {
+        type: "10 Year Term",
+        val: "02",
+      },
+      {
+        type: "15 Year Term",
+        val: "03",
+      },
+      {
+        type: "20 Year Term",
+        val: "04",
+      },
+      {
+        type: "25 Year Term",
+        val: "05",
+      },
+      {
+        type: "30 Year Term",
+        val: "06",
+      },
+      {
+        type: "35 Year Term",
+        val: "07",
+      },
+      {
+        type: "40 Year Term",
+        val: "08",
+      },
+      {
+        type: "Level term to 65",
+        val: "09",
+      },
+      {
+        type: "Level term to 70",
+        val: "010",
+      },
+      {
+        type: "Unique Term",
+        val: "012",
+      },
+      {
+        type: "OptiTerm",
+        val: "1",
+      },
+      {
+        type: "Traditional Term 100",
+        val: "20",
+      },
+      {
+        type: "T100 20 Pay",
+        val: "21",
+      },
+      {
+        type: "Unique Term 100",
+        val: "22",
+      },
+      {
+        type: "Whole life/ life pay",
+        val: "30",
+      },
+      {
+        type: "Whole life/ life pay to 65",
+        val: "31",
+      },
+      {
+        type: "Whole life/ 25 pay",
+        val: "32",
+      },
+      {
+        type: "Whole life/ 20 pay",
+        val: "33",
+      },
+      {
+        type: "Whole life/ 15 pay",
+        val: "34",
+      },
+      {
+        type: "Unique Whole Life",
+        val: "39",
+      },
+      {
+        type: "Living Benefits",
+        val: "5",
+      },
+      {
+        type: "Single person funeral plan",
+        val: "80",
+      },
+    ];
+    const provinceTable = [
+      {
+        province: "Albert",
+        value: "0",
+      },
+      {
+        province: "Saskatchewan",
+        value: "1",
+      },
+      {
+        province: "NWT",
+        value: "3",
+      },
+      {
+        province: "Yukon",
+        value: "4",
+      },
+      {
+        province: "Nunavit",
+        value: "5",
+      },
+      {
+        province: "Manitoba",
+        value: "6",
+      },
+      {
+        province: "Ontario",
+        value: "7",
+      },
+      {
+        province: "Quebec",
+        value: "8",
+      },
+      {
+        province: "NS",
+        value: "9",
+      },
+      {
+        province: "NB",
+        value: "10",
+      },
+      {
+        province: "PEI",
+        value: "11",
+      },
+      {
+        province: "Newfoundland",
+        value: "12",
+      },
+      {
+        province: "All Provinces",
+        value: "13",
+      },
+    ];
+    const countriesTable = [];
+    const smokerTypeTable = [
+      {
+        type:  ,
+        value:  ,
+      },
+    ];
+    const smokerLevelTable = [
+      {
+        frequency: "Never",
+        value: "100",
+      },
+      {
+        frequency: "within 12 months",
+        value: "0",
+      },
+      {
+        frequency: "More than 1 years ago",
+        value: "1",
+      },
+      {
+        frequency: "More than 2 years ago",
+        value: "2",
+      },
+      {
+        frequency: "More than 3 years ago",
+        value: "3",
+      },
+      {
+        frequency: "More than 4 years ago",
+        value: "5",
+      },
+      {
+        frequency: "More than 5 years ago",
+        value: "5",
+      },
+      {
+        frequency: "More than 6 years ago",
+        value: "6",
+      },
+      {
+        frequency: "More than 10 years ago",
+        value: "10",
+      },
+      {
+        frequency: "More than 15 years ago",
+        value: "15",
+      },
+      {
+        frequency: "More than 25 years ago",
+        value: "25",
+      },
+    ];
     // user details form validations
     this.userDetailsForm = this.fb.group({
-      premium: [250000, Validators.required ],
+        birthDay: new FormControl('01', [Validators.required]),
+        birthMonth: new FormControl('01', [Validators.required]),
+        birthYear: new FormControl('1980', [Validators.required]),
+        coverageType: new FormControl(coverageTypeTable[0].val, [Validators.required]),
+        gender: new FormControl("Male", [Validators.required]),
+        premium: new FormControl(250000, [Validators.required]),
+        smokerChew: new FormControl(false, []),
+        smokerCigar: new FormControl(false, []),
+        smokerCigarette: new FormControl(false, []),
+        smokerCigarillo: new FormControl(false, []),
+        smokerPipe: new FormControl(false, []),
+        smokerSnuff: new FormControl(false, []),
+        marijuana: new FormControl(false, []),
+        prescriptionMarijuana: new FormControl(false, []),
+        province: new FormControl(provinceTable[12].province, [Validators.required]), // Ontario is 13
+        tobaccoUse: new FormControl(false, [Validators.required]),
+
     });
   }
 
   public findMoney(value) {
-    const findDollar = new RegExp('\\$\\d+(?:(\\d+))?', 'g');
+    const findDollar = new RegExp("\\$\\d+(?:(\\d+))?", "g");
     const val = value.match(findDollar);
     alert(val);
   }
 
-  public chunkArray(myArray, chunk_size){
-    let arrayLength = myArray.length;
-    let tempArray = [];
+  public chunkArray(myArray, chunk_size) {
+    const arrayLength = myArray.length;
+    const tempArray = [];
     for (let index = 0; index < arrayLength; index += chunk_size) {
-      let myChunk = myArray.slice(index, index + chunk_size);
+      const myChunk = myArray.slice(index, index + chunk_size);
       // Do something if you want with the group
       tempArray.push(myChunk);
     }
@@ -45,20 +262,52 @@ export class FormsComponent implements OnInit {
   }
 
   public winQuoteFunction(value) {
-    let post: Observable<any>;
-    const destinationUrl = 'https://www.winquote.net/compete.pl';
-    const premium = value.premium;
-    const beginBody = 'month=01&day=15&year=1981&First+Client+Gender=1&First+Client+Tobacco+Use=N&fp_smoker_cigarette=100&fp_smoker_cigar=100&fp_smoker_pipe=100&fp_smoker_snuff=100&fp_smoker_cigarello=100&fp_smoker_chew=100&fp_smoker_marijuana=100&fp_smoker_prescribe=100&Province=13&First+Client+Premium=';
-    const endOfBody = '&Payment+Mode=0&First+Client+Coverage+Type=04&First+Client+Plan+Type+OT=0&First+Client+Plan+Type+LB=0&First+Client+Risk=1&type=single&co_code=ca1122573208&co_name=WinQuote&report_type=rank&version_code=ca%3Ase%3A1.3&lang_code=en';;
-    const constructedBody = beginBody + premium + endOfBody;
-    const httpOptions = {
+     let post: Observable<any>;
+     const destinationUrl = "https://www.winquote.net/compete.pl";
+     const birthDay = value.birthDay;
+     const birthMonth = value.birthMonth;
+     const birthYear = value.birthYear;
+     const coverageType = value.coverageType;
+     const gender = value.gender = "Male" ? "1" : "2";
+     const premium = value.premium;
+     const smokerChew = value.smokerChew = true ? "100" : "100";
+     const smokerCigar = value.smokerCigar = true ? "100" : "100";
+     const smokerCigarello = value.smokerCigarillo = true ? "100" : "100";
+     const smokerCigarette = value.smokerCigarette = true ? "100" : "100";
+     const smokerPipe = value.smokerPipe = true ? "100" : "100";
+     const smokerSnuff = value.smokerSnuff = true ? "100" : "100";
+     const marijuana = value.marijuana = true ? "100" : "100";
+     const prescriptionMarijuana = value.prescriptionMarijuana = true ? "100" : "100";
+     const province = value.province;
+     const tobaccoUse = value.tobaccoUse = true ? "Y" : "N";
+
+     const beginBody = "month=01&day=15&year=1981&First+Client+Gender=1&First+Client+Tobacco+Use=N&fp_smoker_cigarette=100&fp_smoker_cigar=100&fp_smoker_pipe=100&fp_smoker_snuff=100&fp_smoker_cigarello=100&fp_smoker_chew=100&fp_smoker_marijuana=100&fp_smoker_prescribe=100&Province=13&First+Client+Premium=";
+     const endOfBody = "&Payment+Mode=0&First+Client+Coverage+Type=04&First+Client+Plan+Type+OT=0&First+Client+Plan+Type+LB=0&First+Client+Risk=1&type=single&co_code=ca1122573208&co_name=WinQuote&report_type=rank&version_code=ca%3Ase%3A1.3&lang_code=en";
+     const monthUrl = "month=" + birthMonth + "&";
+     const dayUrl = "day=" + birthDay + "&";
+     const yearUrl = "year=" + birthYear + "&";
+     const genderUrl = "First+Client+Gender=" + gender + "&";
+     const tobaccoUrl = "First+Client+Tobacco+Use=" + tobaccoUse + "&";
+     const smokerCigaretteUrl =  "fp_smoker_cigarette=" + smokerCigarette + "&";
+     const smokerSnuffUrl = "fp_smoker_snuff=" + smokerSnuff + "&";
+     const smokerPipeUrl = "fp_smoker_snuff=" + smokerPipe + "&";
+     const smokerCigarUrl = "fp_smoker_cigar=" + smokerCigar + "&";
+     const smokerCigarelloUrl = "fp_smoker_cigarello=" + smokerCigarello + "&";
+     const smokerChewUrl = "fp_smoker_chew=" + smokerChew + "&";
+     const smokerMarijuanaUrl =  "fp_smoker_marijuana=" + marijuana + "&";
+     const prescriptionMarijuanaUrl = "fp_smoker_prescribe=" + prescriptionMarijuana + "&";
+     const provinceUrl = "Province=" + province + "&";
+     const premiumUrl = "First+Client+Premium=" + premium + "&";
+     const constructedBody = monthUrl + dayUrl + yearUrl + genderUrl + tobaccoUrl + smokerCigaretteUrl + smokerCigarUrl + smokerPipeUrl + smokerSnuffUrl + smokerCigarelloUrl + smokerChewUrl + smokerMarijuanaUrl + prescriptionMarijuanaUrl + provinceUrl + premiumUrl;
+     
+     const httpOptions = {
       headers: new HttpHeaders({
-        contentType: 'text/html',
+        contentType: "text/html",
       }),
       mode: "cors",
     };
-    post = this.httpClient.post(destinationUrl, constructedBody, httpOptions);
-    post.subscribe(
+     post = this.httpClient.post(destinationUrl, constructedBody, httpOptions);
+     post.subscribe(
       (response) => {
         return;
       }, (error) => {
@@ -66,22 +315,22 @@ export class FormsComponent implements OnInit {
         const table = root[1].children[2].children[7].children[1].children[1].children[9].children[4].children[3].children[1].children;
         let tableDataDollarValue = [];
         let finalArrayForDisplay = [];
-        let tableDataWithoutDollarValue = [];
+        const tableDataWithoutDollarValue = [];
         // go into table
         table.forEach((el) => {
           // go into row
-          if (el.name === 'tr') {
+          if (el.name === "tr") {
             el.children.forEach((elChildren) => {
               if (elChildren.children !== undefined) {
                   // push other values into an array we will later merge with dollar value array
                   tableDataWithoutDollarValue.push(elChildren.children[0].data);
               }
               // go into <td>, which is generated to hold the dollar value of the quote
-              if (elChildren.name === 'td') {
+              if (elChildren.name === "td") {
                 elChildren.children.forEach((deepChild) => {
                   if (deepChild.children !== undefined) {
                     // does cell have a string in in the html which will have a dollar value
-                    elChildren = deepChild.children[0].type === 'text' ? deepChild.children[0].data : undefined;
+                    elChildren = deepChild.children[0].type === "text" ? deepChild.children[0].data : undefined;
                     tableDataDollarValue.push(elChildren);
                   }
                 });
@@ -91,7 +340,7 @@ export class FormsComponent implements OnInit {
         });
         // remove the non-relevant data from start of the array
         tableDataDollarValue.forEach((val, index, object) => {
-          if (!val.match('\\$\\d+(?:(\\d+))?')) {
+          if (!val.match("\\$\\d+(?:(\\d+))?")) {
             object.splice(index, 1);
           }
         });
@@ -111,11 +360,10 @@ export class FormsComponent implements OnInit {
         tableDataDollarValue = this.chunkArray(tableDataDollarValue, 2);
         // merge chunked data from 2 arrays together
         finalArrayForDisplay = tableDataDollarValue.map((e, i) => e.concat(filteredTableArray2[i]));
-        console.log('final array', finalArrayForDisplay);
+        console.log("final array", finalArrayForDisplay);
       },
     );
   }
-
 
   // public kinetixFunction(value) {
   //   let kinetixResp;
